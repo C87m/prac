d6f7018ca77109b23a032a4736a137081f27ad90
msd <chika87ma@gmail.com>
2024-10-07 15:34:38 +0900

    update

--- learn_heapq.py
+++ learn_heapq.py
@@ -16,14 +16,22 @@ print(f"ソート前: {list_heap}")
 heapq.heapify(list_heap)
 print(f"優先度付きキュー: {list_heap}")
 
-print("\n")
-
 # 最小値を取り出す
 print(f"1つ取り出す: {heapq.heappop(list_heap)}")
 print(f"優先度付きキュー: {list_heap}")
 
-print("\n")
-
 # 要素を挿入する
 heapq.heappush(list_heap, -4)
 print(f"-4を挿入: {list_heap}")
+
+print("\n")
+
+# 最大値を取り出す場合-1をかける
+list_minus = [3, 5, 2, 6, 8, 0, 1]
+print(f"ソート前のリスト: {list_heap}")
+list_minus = list(map(lambda x: x*(-1), list_minus))
+heapq.heapify(list_minus)
+print(f"優先度付きキュー: {list_minus}")
+
+print(f"最大値を取り出す: {heapq.heappop(list_minus)*(-1)}")
+print(f"優先度付きキュー: {list_minus}")
